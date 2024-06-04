df797ca41cc7935b44bcf6a73f1661aa3495816c
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2024-06-04 21:50:18 +0100

    initial commit

---
+++ front-end/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>HomePage
+        <div>
+            <img src='https://th.bing.com/th/id/R.c59e1540aa5fe136fa7cc6821bb7adb5?rik=QpdDIHNXYdQuZg&pid=ImgRaw&r=0' alt='cofee' height={600}></img>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
